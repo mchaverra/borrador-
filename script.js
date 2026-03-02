95915ef8bef2e2955a9f70f659ed785636eb551f
mchaverra <m.chaverraa@uniandes.edu.co>
2026-03-02 16:49:01 +0000

    scripttttttt

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hola");
+//console.log("hola");
+
+const varTitulo = document.getElementById(titulo);
+
+console.log(varTitulo)
+varTitulo.addEventListener("click", ()=>{
+    varTitulo.innerText= "Buenos días"
+    varTitulo.style.background= "red"
+})
