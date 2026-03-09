cc6cc6cae83427c62628d2a35f26dfe364b22316
mchaverra <m.chaverraa@uniandes.edu.co>
2026-03-09 15:47:40 +0000

    historia

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //console.log("hola");
 
-const varTitulo = document.getElementById(titulo);
+const varTitulo = document.getElementById("titulo");
+let estadoInicial = true;
 
-console.log(varTitulo)
-varTitulo.addEventListener("click", ()=>{
-    varTitulo.innerText= "Buenos días"
-    varTitulo.style.background= "red"
+varTitulo.addEventListener("click", () => {
+if(estadoInicial === true){
+    varTitulo.innerText="buenos dias";
+    varTitulo.style.color="green";
+    varTitulo.style.backgroundColor ="white";
+} else {
+    varTitulo.innerText = "hola";
+    varTitulo.style.color="red";
+    varTitulo.style.backgroundColor ="transparent";
+}
+estadoInicial = !estadoInicial;
+
+} )
+
+const varTexto= document.getElementById("texto");
+
+const listaTextos=["un","mago","con","una","barita"];
+let indice = 0;
+
+varTexto.addEventListener("click",()=> {
+    if (indice< listaTextos.length){
+        varTexto.innerText= listaTextos[indice];
+    indice = indice + 1;
+} else {
+    //indice=0;// si quiero que vuelvaa empezar
+    varTexto.innerText="FIN.";
+}
 })
